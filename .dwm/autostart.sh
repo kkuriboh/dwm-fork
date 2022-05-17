950380d6ac84bafb62fa104cb2fdec46dcf82007
Augusto Pieper <augustoopieper@gmail.com>
2022-05-17 16:05:15 -0300

    new build

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,4 +1,9 @@
-xrandr -s 1440x900 -r 60
-nitrogen --restore
-dwmblocks &
+#dwmblocks &
+slstatus &
+#xrandr -s 1440x900 -r 60
+feh --bg-fill ~/Pictures/wallpapers/banana.png
+killall picom
 picom &
+setxkbmap -model pc105 -layout us -variant intl
+eval $(/usr/bin/gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
---
+++ dwm/themes/tokyo_night.h
@@ -0,0 +1,28 @@
+#ifndef TOKYO_NIGHT_H
+#define TOKYO_NIGHT_H
+
+/* bg - fg */
+static const char background[]      = "#1a1b26";
+static const char foreground[]      = "#a9b1d6";
+
+/* 8 normal colors */
+static const char black[]           = "#32344a";
+static const char red[]             = "#f7768e";
+static const char green[]           = "#9ece6a";
+static const char yellow[]          = "#e0af68";
+static const char blue[]            = "#7aa2f7";
+static const char magenta[]         = "#ad8ee6";
+static const char cyan[]            = "#449dab";
+static const char white[]           = "#787c99";
+
+/* 8 bright colors */
+static const char black_br[]        = "#444b6a";
+static const char red_br[]          = "#ff7a93";
+static const char green_br[]        = "#b9f27c";
+static const char yellow_br[]       = "#ff9e64";
+static const char blue_br[]         = "#7da6ff";
+static const char magenta_br[]      = "#bb9af7";
+static const char cyan_br[]         = "#0db9d7";
+static const char white_br[]        = "#acb0d0";
+
+#endif // TOKYO_NIGHT_H
