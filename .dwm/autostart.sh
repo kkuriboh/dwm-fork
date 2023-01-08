60dcee24cb6f60e7e82ece55e22a752a15c38f20
Augusto Pieper <augustoopieper@gmail.com>
2023-01-08 17:25:52 -0300

    debloated config

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,9 +1,9 @@
 #dwmblocks &
 slstatus &
 #xrandr -s 1440x900 -r 60
-feh --bg-fill ~/Pictures/wallpapers/banana.png
+sh $HOME/.fehbg
 killall picom
 picom &
 setxkbmap -model pc105 -layout us -variant intl
-eval $(/usr/bin/gnome-keyring-daemon --start)
+# eval $(/usr/bin/gnome-keyring-daemon --start)
 export SSH_AUTH_SOCK
--- dwm/config.h
+++ dwm/config.h
@@ -1,42 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int scalepreview       = 4;        /* tag preview scaling */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 16;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int vertpad            = 5;       /* vertical padding of bar */
-static const int sidepad            = 5;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Lekton Nerd Font:size=7" };
-/*static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#32344a";
-static const char col_gray2[]       = "#7da6ff";
-static const char col_gray3[]       = "#f7768e";
-static const char col_gray4[]       = "#444b6a";
-static const char col_cyan[]        = "#24283b";
-*/
-#include "themes/tokyo_night.h"
+
+#include "themes/tokyonight.h"
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { foreground, background, black },
-	[SchemeSel]  = { foreground, background,  black },
-	[SchemeStatus]  = { green_br, background, black }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]  = { background, yellow, black }, // Tagbar left selected {text,background,not used but cannot be empty}
-    [SchemeTagsNorm]  = { yellow, background, black }, // Tagbar left unselected {text,background,not used but cannot be empty}
-    [SchemeInfoSel]  = { green_br, black, black }, // infobar middle  selected {text,background,not used but cannot be empty}
-    [SchemeInfoNorm]  = { foreground, background, black }, // infobar middle  unselected {text,background,not used but cannot be empty}
+	[SchemeNorm]		= { foreground, background, white_br },
+	[SchemeSel]			= { foreground, background,  green_br },
+	[SchemeStatus]		= { green_br, black, black },			// Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]		= { background, yellow, black },		// Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]	= { yellow, background, black },		// Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]		= { black, white_br, black },			// infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]	= { foreground, background, black },	// infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "ﭮ", "", "", "", "", "" };
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -45,16 +35,15 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "development-window",   NULL,       NULL,       0,            1,           -1 },
-    { "jetbrains-studio",     NULL,       NULL,       0,            1,           -1 },
 	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	NULL
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int decorhints  = 1;    /* 1 means respect decoration hints */
+static const int decorhints  = 1;
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -91,17 +80,14 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "ﬦ=", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *powermenu[]  = { "power", NULL };
 static const char *screenshotmenu[]  = { "screenshot", NULL };
 
-static Key keys[] = {
+static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = powermenu } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshotmenu } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -117,31 +103,29 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
 	{ MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_6,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_7,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_8,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_9,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-    { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[7]}},
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[7]}},
     { MODKEY | ShiftMask,           XK_g,      setlayout,      {.v = &layouts[10]}},
     { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[11]}},
-	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_n,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_n,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -160,7 +144,7 @@ static Key keys[] = {
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
@@ -174,4 +158,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
